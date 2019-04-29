d8e52412aa8268e09258dd5c7993dfb9e08e7c83
Gerald <gerald.pst@gmail.com>
2019-04-29 22:06:17 +0700

    Create basic login

---
+++ app.js
@@ -0,0 +1,66 @@
+var express = require('express')
+var path = require('path')
+// var cookieParser = require('cookie-parser')
+var session = require('express-session')
+var bodyParser = require('body-parser')
+var mysql = require('mysql')
+// var nunjucks = require('nunjucks')
+
+var connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : '',
+	database : 'SCS_SKOM'
+});
+
+var app = express()
+
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+// app.get('/', function(request, response) {
+// 	response.sendFile(path.join(__dirname + '/login.html'));
+// });
+
+// app.get('/', function(req, res){
+//     res.sendFile(__dirname + '/public/login.html')
+// });
+
+app.post('/auth', function(request, response) {
+	var username = request.body.username;
+	var password = request.body.password;
+	if (username && password) {
+		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			if (results.length > 0) {
+				request.session.loggedin = true;
+				request.session.username = username;
+				response.redirect('/home');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+app.get('/home', function(request, response) {
+	if (request.session.loggedin) {
+		response.send('Welcome back, ' + request.session.username + '!');
+	} else {
+		response.send('Please login to view this page!');
+	}
+	response.end();
+});
+
+app.listen(4000, function() {
+    console.log('listening on port 4000 !');
+});
