37e89e9e507f151329e40a7b1b1a7aa96503c5cb
subhanmln <subhanmln@users.noreply.github.com>
2019-05-19 21:25:09 +0700

    fix database

--- scs_skom.sql
+++ scs_skom.sql
@@ -1,23 +1,26 @@
 CREATE TABLE `gate` (
-  `id_gate` INT(11) NOT NULL AUTO_INCREMENT,
-  `nama_gate` VARCHAR(100) DEFAULT NULL,
+  `id_gate` int(11) NOT NULL AUTO_INCREMENT,
+  `nama_gate` varchar(100) DEFAULT NULL,
+  `open` time DEFAULT NULL,
+  `close` time DEFAULT NULL,
+  `role` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id_gate`)
-) ENGINE=INNODB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1
 
 CREATE TABLE `log` (
-  `id_log` INT(11) NOT NULL AUTO_INCREMENT,
-  `id_gate_log` INT(11) DEFAULT NULL,
-  `nrp_log` VARCHAR(50) DEFAULT NULL,
-  `message` VARCHAR(255) NOT NULL,
-  `time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `id_log` int(11) NOT NULL AUTO_INCREMENT,
+  `id_gate_log` int(11) DEFAULT NULL,
+  `nrp_log` varchar(50) DEFAULT NULL,
+  `message` varchar(255) NOT NULL,
+  `time` time NOT NULL,
   PRIMARY KEY (`id_log`)
-) ENGINE=INNODB AUTO_INCREMENT=37 DEFAULT CHARSET=latin1
+) ENGINE=InnoDB AUTO_INCREMENT=65 DEFAULT CHARSET=latin1
 
 CREATE TABLE `user` (
-  `id_user` INT(11) NOT NULL AUTO_INCREMENT,
-  `nrp` VARCHAR(50) NOT NULL,
-  `nama` VARCHAR(255) NOT NULL,
-  `role` VARCHAR(100) NOT NULL,
-  `password` VARCHAR(100) NOT NULL,
+  `id_user` int(11) NOT NULL AUTO_INCREMENT,
+  `nrp` varchar(50) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `role` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
   PRIMARY KEY (`id_user`,`nrp`)
-) ENGINE=INNODB AUTO_INCREMENT=22 DEFAULT CHARSET=latin1
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1
